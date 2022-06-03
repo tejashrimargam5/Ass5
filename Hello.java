1749c432eaddb662ec53db5f23c78fd1f184176b
tejashrimargam5 <82674204+tejashrimargam5@users.noreply.github.com>
2022-06-03 11:34:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
   public static void main(String [] args)
   {
-    System.out.print("Hello World! This is Assignment No 5");
+    System.out.print("Hello World! This is Assignment No 10");
   }
 }
